e61b3bf4d92536bcd4a0fe2e410eacb8d76eba8c
Your Name <you@example.com>
2021-02-23 00:02:28 +0100

    Flyttade ned script och saker börjar fungera?

--- js/yep.js
+++ js/yep.js
@@ -1,53 +1,74 @@
 let gameBoard = [
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 2, 0, 3, 0, 4],
+    [5, 0, 6, 0, 7, 0, 8, 0],
+    [0, 9, 0, 10, 0, 11, 0, 12],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0],
+    [13, 0, 14, 0, 15, 0, 16, 0],
+    [0, 17, 0, 18, 0, 19, 0, 20],
+    [21, 0, 22, 0, 23, 0, 24, 0],
 ];
 
-let playerturn = 1; 
+let playerturn = true; 
 let whiteturn = "White's turn";
 let blackturn = "Black's turn";
-let x = false;
+let totalblack = 12; 
+let totalwhite = 12; 
 
-function Playerturn(playerturn)
+function Playerturn()
 {
- if(playerturn % 2 == 0)
- {
-   document.getElementById("playerturn").innerHTML = whiteturn;
- }
- else
- {
-  document.getElementById("playerturn").innerHTML = blackturn;
- }
- playerturn++; 
+ 
 }
 
-function Piececheck()
+const white = [
+  document.querySelector("#w1"),
+  document.querySelector("#w2"),
+  document.querySelector("#w3"),
+  document.querySelector("#w4"),
+
+  document.querySelector("#w5"),
+  document.querySelector("#w6"),
+  document.querySelector("#w7"),
+  document.querySelector("#w8"),
+
+  document.querySelector("#w9"),
+  document.querySelector("#w10"),
+  document.querySelector("#w11"),
+  document.querySelector("#w12")
+]
+
+const black = [
+  document.querySelector("#b13"),
+  document.querySelector("#b14"),
+  document.querySelector("#b15"),
+  document.querySelector("#b16"),
+
+  document.querySelector("#b17"),
+  document.querySelector("#b18"),
+  document.querySelector("#b19"),
+  document.querySelector("#b20"),
+
+  document.querySelector("#b21"),
+  document.querySelector("#b22"),
+  document.querySelector("#b23"),
+  document.querySelector("#b24")
+]
+
+
+
+white[0].addEventListener("click", pieceHovered(white[0]));
+
+
+function pieceHovered()
 {
-  
-}
+ 
+} 
 
-function test()
+
+
+/*
+for(i = 1; i < 12; i++)
 {
-  console.log("testfunktion");
-  x = true;
-  //document.getElementById("test").style.backgroundColor = "red";
+  = document.querySelector("#" + i);
 }
-
-if(x == true)
-{
-  console.log("if");
-  document.getElementById("test").style.backgroundColor = "red";
-}
-
-//document.getElementsByClassName(square).onclick = Piececheck();
-
-//document.getElementsByClassName("square").innerHTML.onclick = "BLYAT"
-
-//document.getElementsByid("test").innerHTML = console.log("HMm");
-
+*/
